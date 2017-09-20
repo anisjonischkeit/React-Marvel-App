84be1e9b5ceb1d765d51ee8b99a629429a651f58
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-09-20 22:07:02 +1000

    stripped out junk from create react app and set up node path

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import SampleComponent from 'components/SampleComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+        <SampleComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
