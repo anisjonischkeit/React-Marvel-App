dde0d6c9e7451195d07a6b28885c21bbfabb3345
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-09-26 10:23:52 +1000

    added TopBar that can open Sidebar

--- src/containers/template/TopBar.js
+++ src/containers/template/TopBar.js
@@ -1,10 +1,31 @@
+// @flow
+
 import React from 'react';
 import AppBar from 'material-ui/AppBar'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default () => (
+import { openSideBar } from 'actions/template/sidebarActions';
+
+type PropsType = {
+  setOpenState: typeof openSideBar
+}
+
+
+
+
+const TopBar = ({ openSideBar }: PropsType) => (
   <AppBar
-    title="The Marvel Universe Graph"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-    onLeftIconButtonTouchTap={() => alert("hello")}
+  title="The Marvel Universe Graph"
+  iconClassNameRight="muidocs-icon-navigation-expand-more"
+  onLeftIconButtonTouchTap={openSideBar}
   />
 )
+
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators({
+    openSideBar
+  }, dispatch)
+});
+
+export default connect(null, mapDispatchToProps)(TopBar)
