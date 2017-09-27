c696b6448d335ca37c83941cff9c689a85e0d3e0
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-09-27 15:19:54 +1000

    used broeser history for router

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Switch, Route, Redirect } from 'react-router';
 import CharacterList from 'containers/mainList/CharacterList';
 import ComicList from 'containers/mainList/CharacterList';
 
 export default () => (
-  <Router>
+  <BrowserRouter>
 		<Switch>
 			<Redirect exact path='/' to='/characters' />
       <Route exact path='/characters' component={CharacterList} />
       <Route exact path='/comics' component={ComicList} />
     </Switch>
-  </Router>
+  </BrowserRouter>
 );
