66153183166123b4c42918baa69fdeade0fd9251
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-10-02 09:23:27 +0100

    changed the sidebar close icon

--- src/components/template/SideBar.js
+++ src/components/template/SideBar.js
@@ -5,7 +5,7 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'components/template/SideBarMenuItem';
 import Divider from 'material-ui/Divider';
 import BurgerMenu from 'material-ui/svg-icons/navigation/menu'
-import StopIcon from 'material-ui/svg-icons/navigation/cancel'
+import StopIcon from 'material-ui/svg-icons/content/clear'
 
 type PropsType = {
   open: boolean,
