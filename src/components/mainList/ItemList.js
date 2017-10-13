7827a9a57626bb632bb87ad27609839a60022e78
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-10-13 21:20:36 +0100

    removed react-list import

--- src/components/mainList/ItemList.js
+++ src/components/mainList/ItemList.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { List } from 'material-ui/List'
 import MarvelListItem from 'components/mainList/ListItem'
 // import Infinite from 'react-infinite'
-import ReactList from 'react-list';
 
 export type PropsType = {
   rawList: Array<{
