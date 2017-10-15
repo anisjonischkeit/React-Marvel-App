cb3326d875a1f254192c176f817c380ce9012f72
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-10-15 21:46:35 +0200

    added option to add private key for testing

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,4 @@
 export const marvelApi = {
-	publicKey: '6536c6866ced2436cd4aea919af05c06'
+	publicKey: '6536c6866ced2436cd4aea919af05c06',
+	privateKey: undefined
 }
--- src/utils/marvelFetch.js
+++ src/utils/marvelFetch.js
@@ -2,9 +2,13 @@ import { marvelApi } from 'settings';
 import md5 from 'md5'
 
 export default (url, queryArgs=[]) => {
-	const ts = Math.floor(Date.now());
-	const hash = md5(ts + marvelApi.privateKey + marvelApi.publicKey)
-	const authStr = `?apikey=${marvelApi.publicKey}`
+	let authStr = `?apikey=${marvelApi.publicKey}`
+	if (marvelApi.privateKey) {
+		const ts = Math.floor(Date.now());
+		const hash = md5(ts + marvelApi.privateKey + marvelApi.publicKey)
+		
+		authStr += `&ts=${ts}&hash=${hash}`
+	}
 
 	let queryStr = ""
 	Object.keys(queryArgs).forEach(key => {
