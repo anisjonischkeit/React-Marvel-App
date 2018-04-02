d1874c46442c5c75f4d382fa118f0124b28b21e9
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2018-04-03 07:30:51 +1000

    made imports less long

--- src/components/template/SideBar.js
+++ src/components/template/SideBar.js
@@ -1,8 +1,8 @@
 // @flow
 
 import * as React from 'react';
-import Drawer from '../../components/template/Drawer';
-import MenuItem from '../../components/template/SideBarMenuItem';
+import Drawer from './Drawer';
+import MenuItem from './SideBarMenuItem';
 import Divider from 'material-ui/Divider';
 import BurgerMenu from 'material-ui/svg-icons/navigation/menu'
 import StopIcon from 'material-ui/svg-icons/content/clear'
